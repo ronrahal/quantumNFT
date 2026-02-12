c5cc2ac7e9b3e05ae2748f34b491f22155c4c6f5
ronrahal <ronrahal@outlook.com>
2026-02-12 14:17:56 +0200

    docs: finalize README and License

--- PKConverter.js
+++ PKConverter.js
@@ -1,7 +1,7 @@
 import bs58 from 'bs58';
 import fs from 'fs';
 
-const base58String = '3yknwJKrDqrJVtyfSCvPFM1fPpNry5hpvBwuwgFByW3M5kwBDbkoepjBpbQ3KLCavLydStTBwkii3pSYL2shEoiS';
+const base58String = 'YOUR_PRIVATE_KEY_IN_BASE58'; // Replace with your actual base58-encoded private key
 const wallet = bs58.decode(base58String);
 fs.writeFileSync('keypair.json', JSON.stringify(Array.from(wallet)));
 console.log("✅ keypair.json created successfully!");
